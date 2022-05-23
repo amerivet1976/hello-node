c886c4b302376e545ea5f24637887702466ad1eb
amerivet1976 <varughese.thomas@codeimmersives.com>
2022-05-22 21:33:52 -0400

    Print Mega hello world

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("hello world");
+
+console.log("HH   HH        111  111                                           lll       dd !!!")
+console.log("HH   HH  eee   111  111   oooo    ww       ww   oooo    rr  rr    111       dd !!!")
+console.log("HHHHHHH ee ee  111  111  oo  oo   ww       ww  oo  oo   rrr    r  111   dddddd !!!")
+console.log("HH   HH eeeee  111  111  oo  oo     ww ww ww   oo  oo   rr        111  dd    dd")
+console.log("HH   HH  eeeee 111  111   oooo       ww  ww     oooo    rr        111   ddddddd !!!")
+
